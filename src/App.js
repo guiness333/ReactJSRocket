2c3834a4dc748b207debfafaa25b10ad11520131
Walber Martins <walber123k@hotmail.com>
2020-07-30 18:07:24 -0300

    Criando Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import Header from './components/Header';
+import './styles.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello Rocketseat</h1>
+      <Header />
     </div>
   );
 }

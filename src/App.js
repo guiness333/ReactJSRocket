eee9774013fe0c085780973de77c4deb690de4d4
Walber Martins <walber123k@hotmail.com>
2020-08-11 17:04:39 -0300

    Header em styledcomponents

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
-import Header from "./components/Header";
+// import Header from "./components/Header";
 import "./styles.css";
+import { Header as SHeader} from './components/Header/Header';
 import Routes from "./routes";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      {/* <Header /> */}
+      <SHeader>JSHunt</SHeader>
       <Routes />
     </div>
   );
